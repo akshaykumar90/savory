fecad453a2904a4cbb9fb84d540d6324f1d5fa8f
Akshay Kumar <akshaykumar90@gmail.com>
2018-11-09 14:42:08 -0800

    Add an input box for adding tags

--- src/components/TagsRow.vue
+++ src/components/TagsRow.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="flex flex-wrap">
+  <div class="flex flex-wrap items-end pt-2">
     <span v-for="(tag, index) in innerTags" :key="index"
-          class="bg-grey-lighter text-teal-dark p-1 m-1 text-center text-xs rounded cursor-pointer border border-teal-dark">
+          class="bg-grey-lighter text-teal-dark p-1 mr-2 text-center text-xs rounded cursor-pointer border border-teal-dark">
       <span>{{ tag }}</span>
     </span>
+    <input type="text" title="new-tag"
+           class="block text-teal-darker font-xs bg-grey-lighter focus:bg-grey-light focus:outline-none rounded px-2 py-2 h-6">
   </div>
 </template>
 
