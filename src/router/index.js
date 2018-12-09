56bf6f7b9e5e78b9a6b1fde0dbf532ec2b2879c6
Akshay Kumar <akshaykumar90@gmail.com>
2018-12-08 23:06:18 -0800

    Search bar has button to clear search text
    - Clear search bar on route updates

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,10 @@ Vue.use(Router)
 export function createRouter () {
   return new Router({
     routes: [
-      { path: '/tag/:tag', component: require('../App.vue') },
-      { path: '/site/:site', component: require('../App.vue') },
-      { path: '/q/:query', component: require('../App.vue') },
-      { path: '/', component: require('../App.vue') }
+      { path: '/tag/:tag', name: 'tag', component: require('../App.vue') },
+      { path: '/site/:site', name: 'site', component: require('../App.vue') },
+      { path: '/q/:query', name: 'search', component: require('../App.vue') },
+      { path: '/', name: 'home', component: require('../App.vue') }
     ]
   })
 }
