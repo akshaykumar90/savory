abf2307d23992bf3017a62ac3db30731cef227f5
Akshay Kumar <akshaykumar90@gmail.com>
2020-05-03 20:16:16 +0530

    New landing page

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-full h-screen bg-blue-700 bg-cover">
+    <nav class="w-full bg-gray-100">
+      <div class="mx-24 flex justify-between items-center">
+        <a href="#">
+          <img class="h-16" src="../assets/logo_light.svg" alt="logo">
+        </a>
+        <div>
+          <a href="#" class="hover:underline">Sign In</a>
+        </div>
+      </div>
+    </nav>
+    <main class="mx-64 pt-24 flex flex-col items-end">
+      <div class="max-w-xs">
+        <h2 class="text-5xl leading-none tracking-tight font-semibold text-gray-300">Start Organizing</h2>
+        <div class="mt-6 select-none">
+          <a href="#" class="block w-full rounded-lg px-6 py-4 text-lg leading-6 bg-indigo-900 text-gray-300 text-center">Create an Account</a>
+        </div>
+        <p class="text-sm leading-5 mt-4 font-medium text-gray-200">Already have an account? <a href="#" class="underline">Sign In</a></p>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'landing-page'
+  }
+</script>
