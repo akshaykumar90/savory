ecab3897087ed6753babc6ea2214987f1144a35e
Akshay Kumar <akshaykumar90@gmail.com>
2021-02-19 15:49:15 +0530

    nope works

--- src/components/TagsRow.vue
+++ src/components/TagsRow.vue
@@ -37,7 +37,7 @@
       <input
         ref="input"
         type="text"
-        autocomplete="off"
+        autocomplete="nope"
         title="Click to add or remove tags"
         v-model="newTag"
         @keydown.tab.prevent="onTab"
