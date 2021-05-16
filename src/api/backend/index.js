8c58578e6429f77c7e7ca2406a66b53091258b4e
Akshay Kumar <akshaykumar90@gmail.com>
2021-05-16 15:51:55 +0530

    All api functions are now listed

--- src/api/backend/index.js
+++ src/api/backend/index.js
@@ -1,45 +1,66 @@
 import axios from 'axios'
 
-function isLoggedIn() {}
+const instance = axios.create({
+  // `baseURL` will be prepended to `url` unless `url` is absolute.
+  // It can be convenient to set `baseURL` for an instance of axios to pass relative URLs
+  // to methods of that instance.
+  baseURL: process.env.API_BASE_URL,
 
-function login(token) {}
+  // `timeout` specifies the number of milliseconds before the request times out.
+  // If the request takes longer than `timeout`, the request will be aborted.
+  timeout: 3000, // default is `0` (no timeout)
 
-function logout() {}
+  // `withCredentials` indicates whether or not cross-site Access-Control requests
+  // should be made using credentials
+  withCredentials: true,
 
-function refreshToken() {}
+  // `xsrfCookieName` is the name of the cookie to use as a value for xsrf token
+  xsrfCookieName: 'XSRF-TOKEN', // default
 
-function handleAuthFailure() {
-  // call refreshToken above
+  // `xsrfHeaderName` is the name of the http header that carries the xsrf token value
+  xsrfHeaderName: 'X-XSRF-TOKEN', // default
+})
+
+export function login(token) {
+  return instance.post('/login/access-token', { token })
+}
+
+export function logout() {
+  return instance.post('/logout')
+}
+
+export function refreshToken() {
+  return instance.post('/refresh')
 }
 
 export function fetchRecent({ num, before }) {
-  return axios.post('/bookmarks', { num, before })
+  return instance.post('/bookmarks', { num, before })
 }
 
 export function getBookmarksWithTag({ tags, site, num, after }) {
-  return axios.post('/bookmarks', { tags, site, num, after })
+  return instance.post('/bookmarks', { tags, site, num, after })
 }
 
 export function searchBookmarks({ query, num, skip, site, tags }) {
-  return axios.post('/bookmarks/search', { query, num, skip, site, tags })
+  return instance.post('/bookmarks/search', { query, num, skip, site, tags })
 }
 
 export function getTagsCount() {
-  return axios.get('/tags')
+  return instance.get('/tags')
 }
 
 export function createBookmark({ bookmark }) {
-  return axios.post('/bookmarks/add', bookmark)
+  return instance.post('/bookmarks/add', bookmark)
 }
 
-export async function importBookmarks({ chunk }) {
-  return axios.post('/bookmarks/import', {
+export function importBookmarks({ chunk }) {
+  return instance.post('/bookmarks/import', {
     bookmarks: chunk,
   })
 }
 
-export async function deleteBookmarks({ bookmarkIds }) {
-  return axios.delete('/bookmarks', {
+export function deleteBookmarks({ bookmarkIds }) {
+  return instance.delete('/bookmarks', {
     data: {
       bookmark_ids: bookmarkIds,
     },
@@ -47,21 +68,21 @@ export async function deleteBookmarks({ bookmarkIds }) {
 }
 
 export function addTag({ bookmarkId, newTag }) {
-  return axios.post('/tags', {
+  return instance.post('/tags', {
     name: newTag,
     bookmark_id: bookmarkId,
   })
 }
 
 export function bulkAddTag({ bookmarkIds, newTag }) {
-  return axios.post('/tags/bulk', {
+  return instance.post('/tags/bulk', {
     name: newTag,
     bookmark_ids: bookmarkIds,
   })
 }
 
 export function removeTag({ bookmarkId, tagToRemove }) {
-  return axios.delete('/tags', {
+  return instance.delete('/tags', {
     data: {
       name: tagToRemove,
       bookmark_id: bookmarkId,
@@ -70,7 +91,7 @@ export function removeTag({ bookmarkId, tagToRemove }) {
 }
 
 export function bulkRemoveTag({ bookmarkIds, tagToRemove }) {
-  return axios.delete('/tags/bulk', {
+  return instance.delete('/tags/bulk', {
     data: {
       name: tagToRemove,
       bookmark_ids: bookmarkIds,
@@ -79,11 +100,11 @@ export function bulkRemoveTag({ bookmarkIds, tagToRemove }) {
 }
 
 export function loadUserData() {
-  return axios.get('/users/me')
+  return instance.get('/users/me')
 }
 
 export function markBookmarksImported() {
-  return axios.put('/users/me', {
+  return instance.put('/users/me', {
     is_chrome_imported: true,
   })
 }
