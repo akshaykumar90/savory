fdc6ca5df22eca3f4ede05ccb29169a6c78acdaa
Akshay Kumar <akshaykumar90@gmail.com>
2021-07-10 12:49:33 +0530

    Make tailwind work with storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,15 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
 }
