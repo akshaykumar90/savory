44bcdbc96d81ea90741b96c2a57d1303c67aed69
Akshay Kumar <akshaykumar90@gmail.com>
2021-07-17 14:19:07 +0530

    Add tags-row story

--- src/test/fixtures.js
+++ src/test/fixtures.js
@@ -2,12 +2,24 @@ import Vuex from 'vuex'
 
 export function createStore() {
   return new Vuex.Store({
+    state: {
+      list: {
+        activeType: 'new',
+      },
+    },
     getters: {
-      tagsCount: () => {
+      tagsCount: (state) => {
         return [
           ['hello', 1],
           ['world', 1],
         ]
+      },
+      getBookmarkById: (state) => () => {
+        return {
+          id: '1234',
+          site: 'savory.test',
+          tags: ['small', 'long tag with spaces'],
+        }
       },
     },
   })
