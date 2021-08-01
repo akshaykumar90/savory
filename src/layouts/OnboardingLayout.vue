ef6ea8345b85c1d4b518c32dead517efdab585ad
Akshay Kumar <akshaykumar90@gmail.com>
2021-08-01 19:12:05 +0530

    Make onboarding container a layout

---
+++ src/layouts/OnboardingLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col items-center text-center py-8">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'onboarding-layout',
+}
+</script>
--- src/pages/onboarding/OnboardingContainer.vue
+++
@@ -1,9 +0,0 @@
-<template></template>
-
-<script>
-export default {
-  name: 'onboarding-container',
-}
-</script>
-
-<style scoped></style>
