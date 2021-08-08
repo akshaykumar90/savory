ee647993edb6ac8016aed85ba3d16b377503826b
Akshay Kumar <akshaykumar90@gmail.com>
2021-08-08 22:02:50 +0530

    Include screencap for send feedback button

--- src/pages/onboarding/NeedHelp.vue
+++ src/pages/onboarding/NeedHelp.vue
@@ -4,8 +4,13 @@
       Need help?
     </h3>
     <p class="text-lg lg:text-xl text-gray-700 mb-4 lg:w-3/4">
-      If you have a question or want to submit any feedback, the send feedback
-      button in the app will come in handy.
+      If you have a question or want to submit any feedback, use the
+      <img
+        class="h-12 inline"
+        src="https://res.cloudinary.com/savory/image/upload/v1628437717/app/onboarding/feedback-btn_fspsua.png"
+        alt="Bugs & Feedback"
+      />
+      button in the bottom right corner of the app.
     </p>
     <router-link
       to="/welcome/5"
