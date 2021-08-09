106508b545d38269cae04a026194c315ee9b08db
Akshay Kumar <akshaykumar90@gmail.com>
2021-08-09 11:57:23 +0530

    Fire signup success event from the new login callback component

--- src/pages/LoginCallback.js
+++ src/pages/LoginCallback.js
@@ -1,3 +1,5 @@
+import { EVENT_SIGNUP_SUCCESS, eventLogger } from '../api/events'
+
 export default {
   name: 'LoginCallback',
 
@@ -10,7 +12,13 @@ export default {
   async beforeMount() {
     try {
       let resp = await ApiClient.loadUserData()
-      const { show_onboarding } = resp.data
+      const { login_count, show_onboarding } = resp.data
+      if (login_count === 1) {
+        // We provide `/provider_cb` as the success callback to Auth0 which
+        // is handled by this component. If this is the first login for this
+        // user, this must be a successful signup event.
+        eventLogger.logEvent(EVENT_SIGNUP_SUCCESS)
+      }
       if (show_onboarding) {
         this.$router.replace('/welcome')
         ApiClient.markOnboardingSeen()
