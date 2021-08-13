be36e473cad5235a47a6fc9a849be975da2c17d0
Akshay Kumar <akshaykumar90@gmail.com>
2021-08-13 17:28:57 +0530

    Extend backend client to do exponential backoff retries on timeouts (#42)
    Backend API requests sometime time out. This PR implements retries to handle timeout errors on the client.
    
    Not all requests are safe to retry -- so we have introduced an `urlsToRetry` config option to opt-in.
    
    This PR opts-in the "create new bookmark" API operation (`/bookmarks/add`). It is probably NOT safe to retry but let's collect some data before making it idempotent.
    
    Exponential backoff code adapted from [vercel/swr][swr].
    
    [swr]: https://github.com/vercel/swr/blob/3bbbf5b86be0868963c12906816a7bc1ded84c15/src/utils/config.ts#L29

---
+++ src/api/backend/client.test.js
@@ -0,0 +1,177 @@
+const http = require('http')
+import { Client } from './client'
+
+function getFakeAuth() {
+  const mockExpireToken = jest.fn()
+  const mockRefreshToken = jest.fn(() => Promise.resolve())
+  let fakeAuth = {
+    expireToken: mockExpireToken,
+    tryRefreshToken: mockRefreshToken,
+  }
+  return {
+    fakeAuth,
+    mockExpireToken,
+    mockRefreshToken,
+  }
+}
+
+const snooze = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+function printExpBackoff(requestTimes) {
+  let gaps = []
+  let [prev, ...rest] = requestTimes
+  rest.forEach((t) => {
+    gaps.push(t - prev)
+    prev = t
+  })
+  console.log(gaps)
+}
+
+describe('backend server retry handling', () => {
+  let server
+
+  afterEach(() => {
+    server.close()
+  })
+
+  test('happy case', async () => {
+    server = http
+      .createServer(function (req, res) {
+        res.writeHead(200)
+        res.end('Hello, World!')
+      })
+      .listen(8182)
+    const { fakeAuth, mockExpireToken, mockRefreshToken } = getFakeAuth()
+    const config = {
+      baseURL: 'http://localhost:8182',
+      timeout: 100,
+    }
+    const testClient = new Client(fakeAuth, config)
+    let resp = await testClient.loadUserData()
+    expect(resp.status).toEqual(200)
+    expect(mockExpireToken.mock.calls.length).toBe(0)
+    expect(mockRefreshToken.mock.calls.length).toBe(0)
+    expect(testClient.requestTimes.length).toBe(1)
+  })
+
+  it('fails after trying to refresh token on auth error', async () => {
+    server = http
+      .createServer(function (req, res) {
+        res.writeHead(403)
+        res.end('Logged out!')
+      })
+      .listen(8182)
+    const { fakeAuth, mockExpireToken, mockRefreshToken } = getFakeAuth()
+    const config = {
+      baseURL: 'http://localhost:8182',
+      timeout: 100,
+    }
+    const testClient = new Client(fakeAuth, config)
+    await expect(testClient.loadUserData()).rejects.toThrow(
+      /failed with status code 403/
+    )
+    expect(mockExpireToken.mock.calls.length).toBe(1)
+    expect(mockRefreshToken.mock.calls.length).toBe(1)
+    expect(testClient.requestTimes.length).toBe(2)
+  })
+
+  it('succeeds when backend gives new token on auth error', async () => {
+    let requestCount = 0
+    server = http
+      .createServer(function (req, res) {
+        if (requestCount === 0) {
+          requestCount += 1
+          res.writeHead(403)
+          res.end('Logged out!')
+        } else {
+          res.writeHead(200)
+          res.end('Hello, World!')
+        }
+      })
+      .listen(8182)
+    const { fakeAuth, mockExpireToken, mockRefreshToken } = getFakeAuth()
+    const config = {
+      baseURL: 'http://localhost:8182',
+      timeout: 100,
+    }
+    const testClient = new Client(fakeAuth, config)
+    let resp = await testClient.loadUserData()
+    expect(resp.status).toEqual(200)
+    expect(mockExpireToken.mock.calls.length).toBe(0)
+    expect(mockRefreshToken.mock.calls.length).toBe(1)
+    expect(testClient.requestTimes.length).toBe(2)
+  })
+
+  it('does not retry any other error', async () => {
+    server = http
+      .createServer(async function (req, res) {
+        await snooze(20)
+        res.writeHead(200)
+        res.end('Hello, World!')
+      })
+      .listen(8182)
+    const { fakeAuth, mockExpireToken, mockRefreshToken } = getFakeAuth()
+    const config = {
+      baseURL: 'http://localhost:8182',
+      timeout: 10,
+    }
+    const testClient = new Client(fakeAuth, config)
+    await expect(testClient.loadUserData()).rejects.toThrow(
+      /timeout of 10ms exceeded/
+    )
+    expect(mockExpireToken.mock.calls.length).toBe(0)
+    expect(mockRefreshToken.mock.calls.length).toBe(0)
+    expect(testClient.requestTimes.length).toBe(1)
+  })
+
+  it('does exponential backoff retries when url is allowlisted', async () => {
+    server = http
+      .createServer(async function (req, res) {
+        await snooze(20)
+        res.writeHead(200)
+        res.end('Hello, World!')
+      })
+      .listen(8182)
+    const { fakeAuth, mockExpireToken, mockRefreshToken } = getFakeAuth()
+    const config = {
+      baseURL: 'http://localhost:8182',
+      timeout: 10,
+      urlsToRetry: ['/users/me'],
+      errorRetryInterval: 15,
+    }
+    const testClient = new Client(fakeAuth, config)
+    await expect(testClient.loadUserData()).rejects.toThrow(
+      /timeout of 10ms exceeded/
+    )
+    expect(mockExpireToken.mock.calls.length).toBe(0)
+    expect(mockRefreshToken.mock.calls.length).toBe(0)
+    expect(testClient.requestTimes.length).toBe(4)
+  })
+
+  it('succeeds on third try when doing exponential backoff retries', async () => {
+    let requestCount = 0
+    server = http
+      .createServer(async function (req, res) {
+        requestCount += 1
+        if (requestCount < 3) {
+          await snooze(20)
+        }
+        res.writeHead(200)
+        res.end('Hello, World!')
+      })
+      .listen(8182)
+    const { fakeAuth, mockExpireToken, mockRefreshToken } = getFakeAuth()
+    const config = {
+      baseURL: 'http://localhost:8182',
+      timeout: 10,
+      urlsToRetry: ['/users/me'],
+      errorRetryInterval: 15,
+    }
+    const testClient = new Client(fakeAuth, config)
+    let resp = await testClient.loadUserData()
+    expect(resp.status).toEqual(200)
+    expect(mockExpireToken.mock.calls.length).toBe(0)
+    expect(mockRefreshToken.mock.calls.length).toBe(0)
+    expect(testClient.requestTimes.length).toBe(3)
+  })
+})
