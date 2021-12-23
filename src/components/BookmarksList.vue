36be7622599c3c464bd60f1b63b13a1a1e3eb441
Akshay Kumar <akshaykumar90@gmail.com>
2021-12-23 20:25:10 +0530

    Basic markup

--- src/components/BookmarksList.vue
+++ src/components/BookmarksList.vue
@@ -4,8 +4,14 @@
       class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"
     >
       <div v-for="item in bookmarks" :key="item.id" class="">
-        <h2>{{ item.title }}</h2>
-        <h3>{{ item.site }}</h3>
+        <h3>{{ item.title }}</h3>
+        <span>4h ago, 2 highlights</span>
+        <div class="flex">
+          <a v-for="(tag, index) in item.tags" :key="index">
+            {{ tag }}
+          </a>
+          <a href="#">+</a>
+        </div>
       </div>
     </div>
   </div>
@@ -16,8 +22,7 @@ const bookmarks = [
   {
     date_added: 1639901969084,
     id: 'kr9kAMLx',
-    site: 'web.dev',
-    tags: ['frontend', 'performance', 'nextjs'],
+    tags: ['web.dev', 'frontend', 'performance', 'nextjs'],
     title:
       'How QuintoAndar increased conversion rates and pages per session by improving page performance',
     url: 'https://web.dev/quintoandar/',
@@ -25,24 +30,21 @@ const bookmarks = [
   {
     date_added: 1639901907683,
     id: 'MaAoOg9G',
-    site: 'oreilly.com',
-    tags: ['crypto', 'systems', 'finance', 'history'],
+    tags: ['oreilly.com', 'crypto', 'systems', 'finance', 'history'],
     title: 'Why it’s too early to get excited about Web3 – O’Reilly',
     url: 'https://www.oreilly.com/radar/why-its-too-early-to-get-excited-about-web3/',
   },
   {
     date_added: 1639901903811,
     id: 'AxYKVv7r',
-    site: 'lethain.com',
-    tags: ['working', 'people skills'],
+    tags: ['lethain.com', 'working', 'people skills'],
     title: 'Inspection and the limits of trust. | Irrational Exuberance',
     url: 'https://lethain.com/inspection/',
   },
   {
     date_added: 1639901856654,
     id: 'yag29oex',
-    site: 'twitter.com',
-    tags: ['reading', 'person: dan luu'],
+    tags: ['twitter.com', 'reading', 'person: dan luu'],
     title:
       'Dan Luu on Twitter: "The container throttling problem https://t.co/7VI8EcqdqO https://t.co/ChShyyt9yS" / Twitter',
     url: 'https://twitter.com/danluu/status/1472142011918471170',
