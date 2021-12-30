c8681770f6381c39931ba33d18936d5fd5f6c5a5
Akshay Kumar <akshaykumar90@gmail.com>
2021-12-30 18:54:59 +0530

    First cut of app header and toolbar combo

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,137 @@
+<template>
+  <Disclosure as="nav" class="bg-indigo-600" v-slot="{ open }">
+    <div class="px-2 sm:px-4 lg:px-8">
+      <div class="relative flex items-center justify-between h-16">
+        <!-- Logo section -->
+        <div class="flex items-center px-2 lg:px-0 xl:w-64">
+          <div class="flex-shrink-0">
+            <a href="#">
+              <img
+                class="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-300.svg"
+                alt="Savory"
+              />
+            </a>
+          </div>
+        </div>
+
+        <!-- Search section -->
+        <div class="flex-1 flex justify-center lg:justify-end">
+          <div class="w-full px-2 lg:px-6">
+            <label for="search" class="sr-only">Search bookmarks</label>
+            <div class="relative text-indigo-200 focus-within:text-gray-400">
+              <div
+                class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+              >
+                <SearchIcon class="h-5 w-5" aria-hidden="true" />
+              </div>
+              <input
+                id="search"
+                name="search"
+                class="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-indigo-400 bg-opacity-25 text-indigo-100 placeholder-indigo-200 focus:outline-none focus:bg-white focus:ring-0 focus:placeholder-gray-400 focus:text-gray-900 sm:text-sm"
+                placeholder="Search bookmarks"
+                type="search"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="flex lg:hidden">
+          <!-- Mobile menu button -->
+          <DisclosureButton
+            class="bg-indigo-600 inline-flex items-center justify-center p-2 rounded-md text-indigo-400 hover:text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white"
+          >
+            <span class="sr-only">Open main menu</span>
+            <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <!-- Links section -->
+        <div class="hidden lg:block lg:w-80">
+          <div class="flex items-center justify-end">
+            <!-- Profile dropdown -->
+            <Menu as="div" class="ml-4 relative flex-shrink-0">
+              <div>
+                <MenuButton
+                  class="bg-indigo-700 flex text-sm rounded-full text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white"
+                >
+                  <span class="sr-only">Open user menu</span>
+                  <img
+                    class="h-8 w-8 rounded-full"
+                    src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </MenuButton>
+              </div>
+              <transition
+                enter-active-class="transition ease-out duration-100"
+                enter-from-class="transform opacity-0 scale-95"
+                enter-to-class="transform opacity-100 scale-100"
+                leave-active-class="transition ease-in duration-75"
+                leave-from-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95"
+              >
+                <MenuItems
+                  class="origin-top-right absolute z-10 right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                >
+                  <MenuItem v-slot="{ active }">
+                    <a
+                      href="#"
+                      :class="[
+                        active ? 'bg-gray-100' : '',
+                        'block px-4 py-2 text-sm text-gray-700',
+                      ]"
+                      >View Profile</a
+                    >
+                  </MenuItem>
+                  <MenuItem v-slot="{ active }">
+                    <a
+                      href="#"
+                      :class="[
+                        active ? 'bg-gray-100' : '',
+                        'block px-4 py-2 text-sm text-gray-700',
+                      ]"
+                      >Logout</a
+                    >
+                  </MenuItem>
+                </MenuItems>
+              </transition>
+            </Menu>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="lg:hidden">
+      <div class="pt-4 pb-3 border-t border-indigo-800">
+        <div class="px-2">
+          <DisclosureButton
+            as="a"
+            href="#"
+            class="block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600"
+            >Your Profile</DisclosureButton
+          >
+          <DisclosureButton
+            as="a"
+            href="#"
+            class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600"
+            >Sign out</DisclosureButton
+          >
+        </div>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
+</template>
+
+<script setup>
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from '@headlessui/vue'
+import { SearchIcon } from '@heroicons/vue/solid'
+import { MenuIcon, XIcon } from '@heroicons/vue/outline'
+</script>
---
+++ src/components/TopToolbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="bg-white h-16 flex items-center">
+    <div>1 selected</div>
+    <div class="px-4 sm:px-6 lg:px-8">
+      <div class="py-3 flex justify-between">
+        <div>
+          <span
+            class="relative z-0 inline-flex shadow-sm rounded-md sm:shadow-none sm:space-x-3"
+          >
+            <span class="inline-flex sm:shadow-sm">
+              <button
+                type="button"
+                class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-900 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-blue-600 focus:border-blue-600"
+              >
+                <ReplyIcon
+                  class="mr-2.5 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                <span>Tags</span>
+              </button>
+              <button
+                type="button"
+                class="hidden sm:inline-flex -ml-px relative items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-900 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-blue-600 focus:border-blue-600"
+              >
+                <PencilIcon
+                  class="mr-2.5 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                <span>Edit</span>
+              </button>
+            </span>
+
+            <span class="hidden lg:flex space-x-3">
+              <button
+                type="button"
+                class="hidden sm:inline-flex -ml-px relative items-center px-4 py-2 rounded-md border border-gray-300 bg-white text-sm font-medium text-gray-900 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-blue-600 focus:border-blue-600"
+              >
+                <ArchiveIconSolid
+                  class="mr-2.5 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                <span>Delete</span>
+              </button>
+            </span>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {
+  ArchiveIcon as ArchiveIconSolid,
+  PencilIcon,
+  ReplyIcon,
+} from '@heroicons/vue/solid'
+</script>
