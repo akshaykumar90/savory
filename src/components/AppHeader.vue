11cfe2c440060da6d656243222f248a60469e603
Akshay Kumar <akshaykumar90@gmail.com>
2022-01-01 17:50:28 +0530

    Fix menu on mobile

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -40,7 +40,7 @@
         <div class="flex lg:hidden">
           <!-- Mobile menu button -->
           <DisclosureButton
-            class="bg-white-600 inline-flex items-center justify-center p-2 rounded-md text-black-400 hover:text-white hover:bg-black-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white"
+            class="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             <span class="sr-only">Open main menu</span>
             <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
@@ -112,21 +112,44 @@
     </div>
 
     <DisclosurePanel class="lg:hidden">
-      <div class="pt-4 pb-3 border-t border-indigo-800">
-        <div class="px-2">
-          <DisclosureButton
-            as="a"
-            href="#"
-            class="block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600"
-            >Your Profile</DisclosureButton
-          >
-          <DisclosureButton
-            as="a"
-            href="#"
-            class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600"
-            >Sign out</DisclosureButton
-          >
-        </div>
+      <div class="pt-2 pb-3 space-y-1">
+        <DisclosureButton
+          as="a"
+          href="#"
+          class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+        >
+          Inbox
+        </DisclosureButton>
+        <DisclosureButton
+          as="a"
+          href="#"
+          class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+        >
+          Reading
+        </DisclosureButton>
+        <DisclosureButton
+          as="a"
+          href="#"
+          class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+        >
+          Watching
+        </DisclosureButton>
+      </div>
+      <div class="pt-2 pb-3 border-t border-gray-200 space-y-1">
+        <DisclosureButton
+          as="a"
+          href="#"
+          class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+        >
+          Your Profile
+        </DisclosureButton>
+        <DisclosureButton
+          as="a"
+          href="#"
+          class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+        >
+          Sign out
+        </DisclosureButton>
       </div>
     </DisclosurePanel>
   </Disclosure>
