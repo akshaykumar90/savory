73072b33e873a6d46e8943fc7f6385c9c25a17bb
Akshay Kumar <akshaykumar90@gmail.com>
2022-01-10 21:32:47 +0530

    Page store

--- src/stores/page.js
+++ src/stores/page.js
@@ -4,22 +4,35 @@ export const usePageStore = defineStore('page', {
   state: () => ({
     itemsPerPage: 100,
     page: 1,
-    total: 200,
+    total: 1432,
   }),
   getters: {
+    maxPage(state) {
+      return Math.ceil(state.total / state.itemsPerPage)
+    },
     start(state) {
       return state.itemsPerPage * (state.page - 1) + 1
     },
     end(state) {
-      return this.start + state.itemsPerPage - 1
+      return Math.min(this.start + state.itemsPerPage - 1, state.total)
+    },
+    hasNext(state) {
+      return state.page < this.maxPage
+    },
+    hasPrevious(state) {
+      return state.page > 1
     },
   },
   actions: {
     next() {
-      this.page++
+      if (this.hasNext) {
+        this.page++
+      }
     },
     previous() {
-      this.page--
+      if (this.hasPrevious) {
+        this.page--
+      }
     },
   },
 })
