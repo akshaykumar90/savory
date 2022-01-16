6dbd6c8706ba9796fc4da42bee2ab82aeeec2e45
Akshay Kumar <akshaykumar90@gmail.com>
2022-01-16 13:52:20 +0530

    Cleanup useBookmark composable

--- src/composables/useBookmark.js
+++ src/composables/useBookmark.js
@@ -1,25 +1,9 @@
-import { useQuery, useMutation, useQueryClient } from 'vue-query'
+import { useQuery } from 'vue-query'
 import { reactive } from 'vue'
 
 export function useBookmark(bookmarkId) {
   const key = reactive(['bookmarks', bookmarkId])
   return useQuery(key, () => Promise.resolve({}), {
     staleTime: Infinity,
-  })
-}
-
-export function useAddTag() {
-  const queryClient = useQueryClient()
-
-  return useMutation((values) => ApiClient.addTag(values), {
-    onSuccess: (data, { bookmarkId, newTag }) => {
-      const oldBookmark = queryClient.getQueryData(['bookmarks', bookmarkId])
-
-      const updatedBookmark = {
-        ...oldBookmark,
-        tags: [...oldBookmark.tags, newTag],
-      }
-      queryClient.setQueryData(['bookmarks', bookmarkId], updatedBookmark)
-    },
   })
 }
