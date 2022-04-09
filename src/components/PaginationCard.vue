aa0838e8b1f8dcda19cd51c768c9f2913e37427f
Akshay Kumar <akshaykumar90@gmail.com>
2022-04-09 11:13:19 -0700

    Add button in pagination card to clear filters

--- src/components/PaginationCard.vue
+++ src/components/PaginationCard.vue
@@ -9,33 +9,49 @@
           <span class="font-medium">{{ placemarkMessage }}</span>
         </p>
       </div>
-      <span class="relative z-0 inline-flex rounded-md shadow-sm">
-        <button
-          type="button"
-          class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
-          @click="previousPage"
-          :disabled="isFetching || !data.cursor_info.has_previous_page"
+      <div class="flex space-x-4">
+        <span
+          v-if="showClearFiltersButton"
+          class="relative z-0 inline-flex rounded-md shadow-sm"
         >
-          <span class="sr-only">Previous</span>
-          <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
-        </button>
-        <button
-          type="button"
-          class="relative -ml-px inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
-          @click="nextPage"
-          :disabled="isFetching || !data.cursor_info.has_next_page"
-        >
-          <span class="sr-only">Next</span>
-          <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
-        </button>
-      </span>
+          <button
+            type="button"
+            class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+            @click="clearFilters"
+          >
+            <span class="sr-only">Clear filters</span>
+            <XIcon class="h-5 w-5" aria-hidden="true" />
+          </button>
+        </span>
+        <span class="relative z-0 inline-flex rounded-md shadow-sm">
+          <button
+            type="button"
+            class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+            @click="previousPage"
+            :disabled="isFetching || !data.cursor_info.has_previous_page"
+          >
+            <span class="sr-only">Previous</span>
+            <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
+          </button>
+          <button
+            type="button"
+            class="relative -ml-px inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+            @click="nextPage"
+            :disabled="isFetching || !data.cursor_info.has_next_page"
+          >
+            <span class="sr-only">Next</span>
+            <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
+          </button>
+        </span>
+      </div>
     </nav>
   </div>
 </template>
 
 <script setup>
-import { watch, ref } from 'vue'
+import { watch, ref, computed } from 'vue'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/solid'
+import { XIcon } from '@heroicons/vue/outline'
 
 import { useRouter } from 'vue-router'
 import useBookmarksPage from '../composables/useBookmarksPage'
@@ -67,6 +83,10 @@ watch(
   }
 )
 
+const showClearFiltersButton = computed(
+  () => store.tags.length > 0 || store.site
+)
+
 function nextPage() {
   let cursor = data.value.cursor_info.next_cursor
   store.updateCursor(cursor, router)
@@ -75,5 +95,9 @@ function nextPage() {
 function previousPage() {
   let cursor = data.value.cursor_info.previous_cursor
   store.updateCursor(cursor, router)
+}
+
+function clearFilters() {
+  router.push('/')
 }
 </script>
