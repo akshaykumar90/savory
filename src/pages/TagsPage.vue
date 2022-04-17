ec2b217aabc41dd643666628d77af92d50803aba
Akshay Kumar <akshaykumar90@gmail.com>
2022-04-17 16:37:40 -0700

    The /tags page now has actual links to single tag pages

--- src/pages/TagsPage.vue
+++ src/pages/TagsPage.vue
@@ -1,10 +1,13 @@
 <template>
   <ul role="list" class="divide-y divide-gray-200">
     <li v-for="(value, key) in data" :key="key" class="flex py-4">
-      <div class="mx-4 flex w-full flex-row justify-between">
+      <router-link
+        :to="{ path: '/tag', query: { name: key } }"
+        class="mx-4 flex w-full flex-row justify-between"
+      >
         <span class="text-gray-900 line-clamp-1">{{ key }}</span>
         <span class="text-gray-900">{{ value }}</span>
-      </div>
+      </router-link>
     </li>
   </ul>
 </template>
