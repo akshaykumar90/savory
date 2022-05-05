3b25100a863e71201fa53e4323257df039f9ec2d
Akshay Kumar <akshaykumar90@gmail.com>
2022-05-06 00:15:23 +0530

    Drop tldjs as dependency (#48)

--- src/lib/utils.js
+++
@@ -1,7 +0,0 @@
-const { getDomain } = require('tldjs')
-
-export function domainName(bookmarkURL) {
-  const url = new URL(bookmarkURL)
-  // Drop the subdomain, e.g. news.ycombinator.com -> ycombinator.com
-  return getDomain(url.hostname)
-}
