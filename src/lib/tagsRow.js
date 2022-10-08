7020cca22d98e3f494610317628715473577cd79
Akshay Kumar <akshaykumar90@gmail.com>
2022-10-08 15:18:31 +0530

    Comment

--- src/lib/tagsRow.js
+++ src/lib/tagsRow.js
@@ -1,6 +1,8 @@
 import _ from 'lodash'
 
 export function tagsWithAccentBit(allTags, pageTags) {
+  // Cannot use pullAll here because allTags will actually be a proxy, and it
+  // cannot be mutated.
   const tagsExcluded = _.difference(allTags, pageTags)
   let result = pageTags.map((t) => ({ name: t, accented: true }))
   result = result.concat(
