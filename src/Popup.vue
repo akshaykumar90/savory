095d4a3c960b609c12bed7ed58ceaaaa3552bbf2
Akshay Kumar <akshaykumar90@gmail.com>
2022-10-14 21:30:42 +0530

    Saving via clicking extension works

--- src/Popup.vue
+++ src/Popup.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="h-[260px] w-[300px]">
+  <div class="w-[300px] p-2">
     <div
       v-if="isLoading || isIdle"
-      class="flex h-full items-center justify-center"
+      class="flex min-h-[187px] items-center justify-center"
     >
       <svg
         class="h-5 w-5 animate-spin text-primary"
@@ -40,31 +40,48 @@
         </div>
       </div>
     </div>
-    <div v-else class="rounded-md bg-green-50 p-4">
-      <div class="flex">
-        <div class="flex-shrink-0">
-          <CheckCircleIcon class="h-5 w-5 text-green-400" aria-hidden="true" />
-        </div>
-        <div class="ml-3">
-          <h3 class="text-sm font-medium text-green-800">Added to Savory</h3>
-          <div class="mt-2 text-sm text-green-700">
-            <p>Saved bookmark id is: {{ bookmarkId }}</p>
+    <Popover v-else class="relative">
+      <div class="rounded-md bg-green-50 p-4">
+        <div class="flex">
+          <div class="flex-shrink-0">
+            <CheckCircleIcon
+              class="h-5 w-5 text-green-400"
+              aria-hidden="true"
+            />
           </div>
-          <div class="mt-4">
-            <div class="-mx-2 -my-1.5 flex">
-              <tags-popover :bookmark-id="bookmarkId" :popup="true">
+          <div class="ml-3">
+            <h3 class="text-sm font-medium text-green-800">Added to Savory</h3>
+            <div class="mt-2 text-sm text-green-700">
+              <p>Saved bookmark id is: {{ bookmarkId }}</p>
+            </div>
+            <div class="mt-4">
+              <div class="-mx-2 -my-1.5 flex">
                 <popover-button
                   type="button"
                   class="rounded-md bg-green-50 px-2 py-1.5 text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
                 >
                   <span>Add tags</span>
                 </popover-button>
-              </tags-popover>
+                <button
+                  @click="openSavory"
+                  type="button"
+                  class="ml-3 rounded-md bg-green-50 px-2 py-1.5 text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
+                >
+                  Open Savory
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <PopoverPanel class="mt-2">
+        <div
+          class="overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5"
+        >
+          <edit-tags :bookmark-id="bookmarkId" :popup="true"></edit-tags>
+        </div>
+      </PopoverPanel>
+    </Popover>
   </div>
 </template>
 
@@ -73,8 +90,10 @@ import { useMutation } from '@tanstack/vue-query'
 import { computed, onMounted } from 'vue'
 import { CheckCircleIcon } from '@heroicons/vue/20/solid'
 import { XCircleIcon } from '@heroicons/vue/20/solid'
-import { PopoverButton } from '@headlessui/vue'
-import TagsPopover from './components/TagsPopover.vue'
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
+import EditTags from './components/EditTags.vue'
+
+const savory_app_url = 'https://app.savory.test:8080'
 
 const { isLoading, isIdle, isError, data, error, mutate } = useMutation((tab) =>
   ApiClient.saveTab(tab)
@@ -82,29 +101,25 @@ const { isLoading, isIdle, isError, data, error, mutate } = useMutation((tab) =>
 
 const bookmarkId = computed(() => data.value && data.value.data.id)
 
-// async function saveCurrentTab() {
-//   let queryOptions = { active: true, lastFocusedWindow: true }
-//   // `tab` will either be a `tabs.Tab` instance or `undefined`.
-//   let [tab] = await chrome.tabs.query(queryOptions)
-//   if (tab) {
-//     mutate({
-//       dateAddedMs: Date.now(),
-//       title: tab.title,
-//       url: tab.url,
-//     })
-//   }
-// }
-
-function saveTab() {
-  mutate({
-    dateAddedMs: Date.now(),
-    title: 'Savory is the next $100B app',
-    url: 'https://getsavory.co',
-  })
+async function saveCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true }
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions)
+  if (tab) {
+    mutate({
+      dateAddedMs: Date.now(),
+      title: tab.title,
+      url: tab.url,
+    })
+  }
 }
 
-onMounted(async () => {
-  setTimeout(saveTab, 500)
+function openSavory() {
+  chrome.tabs.create({ url: savory_app_url })
+}
+
+onMounted(() => {
+  saveCurrentTab()
 })
 </script>
 
