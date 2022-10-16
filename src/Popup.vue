f0a36852d2e85f9f510c47f7998e4854b7788d68
Akshay Kumar <akshaykumar90@gmail.com>
2022-10-16 12:07:05 +0530

    Extract spinner into its own component

--- src/Popup.vue
+++ src/Popup.vue
@@ -4,26 +4,7 @@
       v-if="isLoading || isIdle"
       class="flex min-h-[187px] items-center justify-center"
     >
-      <svg
-        class="h-5 w-5 animate-spin text-primary"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
+      <Spinner></Spinner>
     </div>
     <div v-else-if="isError" class="rounded-md bg-red-50 p-4">
       <div class="flex">
@@ -92,6 +73,7 @@ import { CheckCircleIcon } from '@heroicons/vue/20/solid'
 import { XCircleIcon } from '@heroicons/vue/20/solid'
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
 import EditTags from './components/EditTags.vue'
+import Spinner from './components/Spinner.vue'
 
 const savory_app_url = 'https://app.savory.test:8080'
 
@@ -129,7 +111,7 @@ function openSavory() {
 }
 
 onMounted(() => {
-  saveCurrentTab()
+  // saveCurrentTab()
 })
 </script>
 
