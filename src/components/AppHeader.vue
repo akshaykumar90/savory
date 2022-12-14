16845f62b0e578c02c4826c0ede98e89c0813e9c
Akshay Kumar <akshaykumar90@gmail.com>
2022-12-14 14:08:35 +0530

    Put the links in desktop menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -88,6 +88,21 @@
                 <MenuItems
                   class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                 >
+                  <MenuItem
+                    v-for="link in outboundLinks"
+                    :key="link.name"
+                    v-slot="{ active }"
+                  >
+                    <a
+                      :href="link.href"
+                      target="_blank"
+                      :class="[
+                        active ? 'bg-gray-100' : '',
+                        'block px-4 py-2 text-sm text-gray-700',
+                      ]"
+                      >{{ link.name }}</a
+                    >
+                  </MenuItem>
                   <MenuItem v-slot="{ active }">
                     <a
                       href="#"
