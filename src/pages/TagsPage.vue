241331eec13cbd0904b934935158577d3fc0e772
Akshay Kumar <akshaykumar90@gmail.com>
2023-01-13 18:27:32 +0530

    Follow tanstack query best practices to avoid flash of previous data

--- src/pages/TagsPage.vue
+++ src/pages/TagsPage.vue
@@ -1,10 +1,11 @@
 <template>
-  <ErrorScreen v-if="isError" :detail="errorDetail">
+  <div v-if="isLoading">&nbsp;</div>
+  <ErrorScreen v-else-if="isError" :detail="errorDetail">
     <PrimaryButton button-text="Retry" @click="onRetry">
       <ArrowPathIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
     </PrimaryButton>
   </ErrorScreen>
-  <div v-else-if="data">
+  <div v-else>
     <div class="border-b border-gray-200 bg-white p-4">
       <div class="-ml-4 -mt-2 flex flex-nowrap items-center justify-between">
         <div class="ml-4 mt-2 flex-shrink-0">
@@ -46,7 +47,7 @@ import ErrorScreen from '../components/ErrorScreen.vue'
 import PrimaryButton from '../components/PrimaryButton.vue'
 import { ArrowPathIcon } from '@heroicons/vue/20/solid'
 
-const { isError, error, data, refetch: onRetry } = useTags()
+const { isLoading, isError, error, data, refetch: onRetry } = useTags()
 
 const errorDetail = computed(() => {
   if (error.value) {
