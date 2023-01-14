0283524bd8ccafd564c20e012190c78b9884b6d0
Akshay Kumar <akshaykumar90@gmail.com>
2023-01-14 16:17:43 +0530

    Empty state screens (#98)

--- src/components/ErrorScreen.vue
+++ src/components/ErrorScreen.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="py-16 text-center">
+  <div class="py-16 px-4 text-center sm:px-6 lg:px-8">
     <img
-      class="mx-auto w-3/4 px-4 opacity-75 sm:w-1/2"
-      src="../assets/images/walking_outside.svg"
+      class="mx-auto w-3/4 opacity-75 sm:w-1/2"
+      src="../assets/images/meditating.png"
       alt=""
+      width="400"
+      height="314"
     />
     <h3 class="mt-10 text-sm font-medium text-gray-900">{{ errorTitle }}</h3>
     <p class="mt-1 text-sm text-gray-500">{{ errorDetail }}</p>
--- src/pg/Playground.vue
+++ src/pg/Playground.vue
@@ -1,11 +1,11 @@
 <script setup>
-import ErrorScreen from '../components/ErrorScreen.vue'
+import EmptyReading from '../components/EmptyReading.vue'
 </script>
 
 <template>
   <div
     class="mx-auto my-10 max-w-xl space-y-10 border px-4 pt-16 sm:px-6 lg:px-8"
   >
-    <ErrorScreen />
+    <EmptyReading />
   </div>
 </template>
