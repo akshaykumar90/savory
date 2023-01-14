249e4d7223e56078b48175ca17961030b80230e2
Akshay Kumar <akshaykumar90@gmail.com>
2023-01-14 21:23:36 +0530

    Add a loading skeleton for bookmarks page

---
+++ src/components/LoadingSkeleton.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-4">
+    <div class="h-5 w-1/6 rounded-lg bg-blue-100/40"></div>
+    <div class="flex items-start gap-4 pt-8">
+      <div class="h-5 w-5 rounded-lg bg-blue-100/40"></div>
+      <div class="flex-1 space-y-3">
+        <div class="h-5 w-3/5 rounded-lg bg-blue-100/70"></div>
+        <div class="h-5 w-4/5 rounded-lg bg-blue-100/40"></div>
+        <div class="h-5 w-2/5 rounded-lg bg-blue-100/40"></div>
+      </div>
+    </div>
+  </div>
+</template>
