f3f189f0201485f7fee67bcb64e6b919c369af9e
Akshay Kumar <akshaykumar90@gmail.com>
2024-11-03 13:12:22 +0530

    Make all pages redirect to login when auth expires

--- src/app/(bookmarks)/page.tsx
+++ src/app/(bookmarks)/page.tsx
@@ -12,6 +12,7 @@ import ErrorScreen from "./error-screen"
 import PaginationCard from "./pagination-card"
 import { RefreshOnFocus } from "./refresh-on-focus"
 import { WaitForMutations } from "./wait-for-mutations"
+import { withPageAuthRequired } from "@auth0/nextjs-auth0"
 
 export async function generateMetadata({
   searchParams,
@@ -48,128 +49,130 @@ export async function generateMetadata({
   }
 }
 
-export default async function TagPage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined }
-}) {
-  let {
-    name: urlName,
-    site: urlSite,
-    cursor: urlCursor,
-    untagged: urlUntagged,
-    q: urlQuery,
-  } = searchParams
+export default withPageAuthRequired(
+  async function TagPage({
+    searchParams,
+  }: {
+    searchParams?: { [key: string]: string | string[] | undefined }
+  }) {
+    let {
+      name: urlName,
+      site: urlSite,
+      cursor: urlCursor,
+      untagged: urlUntagged,
+      q: urlQuery,
+    } = searchParams ?? {}
 
-  const tags = !urlName ? [] : Array.isArray(urlName) ? urlName : [urlName]
-  const site = Array.isArray(urlSite) ? urlSite[0] : urlSite
-  const query = Array.isArray(urlQuery) ? urlQuery[0] : urlQuery
-  const cursor = Array.isArray(urlCursor) ? urlCursor[0] : urlCursor
-  const untagged = Array.isArray(urlUntagged)
-    ? Boolean(urlUntagged[0])
-    : Boolean(urlUntagged)
+    const tags = !urlName ? [] : Array.isArray(urlName) ? urlName : [urlName]
+    const site = Array.isArray(urlSite) ? urlSite[0] : urlSite
+    const query = Array.isArray(urlQuery) ? urlQuery[0] : urlQuery
+    const cursor = Array.isArray(urlCursor) ? urlCursor[0] : urlCursor
+    const untagged = Array.isArray(urlUntagged)
+      ? Boolean(urlUntagged[0])
+      : Boolean(urlUntagged)
 
-  const queryClient = new QueryClient()
+    const queryClient = new QueryClient()
 
-  const commonArgs = {
-    ...(site && { site }),
-    ...(tags.length && { tags }),
-    ...(cursor && { cursor }),
-    ...(untagged && { untagged }),
-    num: 25,
-  }
-
-  let tagsResponse, bookmarksResponse, drillDownTagsResponse
-
-  try {
-    if (query) {
-      // Search page
-      let arr = await Promise.all([
-        bapi.getTagsCount(),
-        bapi.searchBookmarks({
-          ...commonArgs,
-          query,
-        }),
-      ])
-      tagsResponse = arr[0]
-      bookmarksResponse = arr[1]
-    } else if (tags.length || site) {
-      // Tag page
-      let arr = await Promise.all([
-        bapi.getTagsCount(),
-        bapi.getBookmarks(commonArgs),
-        bapi.getDrillDownTags({ tags, site }),
-      ])
-      tagsResponse = arr[0]
-      bookmarksResponse = arr[1]
-      drillDownTagsResponse = arr[2]
-    } else {
-      // Home page
-      let arr = await Promise.all([
-        bapi.getTagsCount(),
-        bapi.getBookmarks(commonArgs),
-      ])
-      tagsResponse = arr[0]
-      bookmarksResponse = arr[1]
+    const commonArgs = {
+      ...(site && { site }),
+      ...(tags.length && { tags }),
+      ...(cursor && { cursor }),
+      ...(untagged && { untagged }),
+      num: 25,
     }
-  } catch (error) {
-    // TODO: If error is logout, redirect!
-    return <ErrorScreen />
-  }
 
-  bookmarksResponse.bookmarks.forEach((bookmark) => {
-    queryClient.setQueryData(["bookmarks", bookmark.id], bookmark)
-  })
+    let tagsResponse, bookmarksResponse, drillDownTagsResponse
 
-  queryClient.setQueryData(tagsQuery.queryKey, tagsResponse)
-
-  const drillTags =
-    drillDownTagsResponse?.tags_list
-      // Sort the search results by decreasing tag frequency
-      .sort(({ count: freq1 }, { count: freq2 }) => {
-        return -(freq1 - freq2)
-      })
-      .map(({ name }) => name) ?? []
-
-  const hasUntagged = drillDownTagsResponse?.has_untagged ?? false
-
-  const numTotal = bookmarksResponse.total
-
-  let message
-
-  if (numTotal === 0) {
-    message = "Nothing to see here"
-  } else {
-    const itemsStr = numTotal > 1 ? "bookmarks" : "bookmark"
-    if (tags.length > 0) {
-      message = `${numTotal} ${itemsStr} in ${tags.join(", ")}`
-    } else if (site) {
-      message = `${numTotal} ${itemsStr} in ${site}`
-    } else {
-      message = `${numTotal} ${itemsStr}`
+    try {
+      if (query) {
+        // Search page
+        let arr = await Promise.all([
+          bapi.getTagsCount(),
+          bapi.searchBookmarks({
+            ...commonArgs,
+            query,
+          }),
+        ])
+        tagsResponse = arr[0]
+        bookmarksResponse = arr[1]
+      } else if (tags.length || site) {
+        // Tag page
+        let arr = await Promise.all([
+          bapi.getTagsCount(),
+          bapi.getBookmarks(commonArgs),
+          bapi.getDrillDownTags({ tags, site }),
+        ])
+        tagsResponse = arr[0]
+        bookmarksResponse = arr[1]
+        drillDownTagsResponse = arr[2]
+      } else {
+        // Home page
+        let arr = await Promise.all([
+          bapi.getTagsCount(),
+          bapi.getBookmarks(commonArgs),
+        ])
+        tagsResponse = arr[0]
+        bookmarksResponse = arr[1]
+      }
+    } catch (error) {
+      return <ErrorScreen />
     }
-  }
 
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="flex flex-col">
-        <PaginationCard
-          showClearFiltersButton={tags.length > 0 || !!site || !!query}
-          message={message}
-          nextCursor={bookmarksResponse.cursor_info.next_cursor}
-          prevCursor={bookmarksResponse.cursor_info.previous_cursor}
-        />
-        {(drillTags.length > 0 || hasUntagged) && (
-          <DrillDownCard tags={drillTags} showUntagged={hasUntagged} />
-        )}
-        <ul className="flex flex-col">
-          {bookmarksResponse.bookmarks.map((bookmark) => (
-            <BookmarkRow key={bookmark.id} bookmarkId={bookmark.id} />
-          ))}
-        </ul>
-      </div>
-      <RefreshOnFocus />
-      <WaitForMutations />
-    </HydrationBoundary>
-  )
-}
+    bookmarksResponse.bookmarks.forEach((bookmark) => {
+      queryClient.setQueryData(["bookmarks", bookmark.id], bookmark)
+    })
+
+    queryClient.setQueryData(tagsQuery.queryKey, tagsResponse)
+
+    const drillTags =
+      drillDownTagsResponse?.tags_list
+        // Sort the search results by decreasing tag frequency
+        .sort(({ count: freq1 }, { count: freq2 }) => {
+          return -(freq1 - freq2)
+        })
+        .map(({ name }) => name) ?? []
+
+    const hasUntagged = drillDownTagsResponse?.has_untagged ?? false
+
+    const numTotal = bookmarksResponse.total
+
+    let message
+
+    if (numTotal === 0) {
+      message = "Nothing to see here"
+    } else {
+      const itemsStr = numTotal > 1 ? "bookmarks" : "bookmark"
+      if (tags.length > 0) {
+        message = `${numTotal} ${itemsStr} in ${tags.join(", ")}`
+      } else if (site) {
+        message = `${numTotal} ${itemsStr} in ${site}`
+      } else {
+        message = `${numTotal} ${itemsStr}`
+      }
+    }
+
+    return (
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <div className="flex flex-col">
+          <PaginationCard
+            showClearFiltersButton={tags.length > 0 || !!site || !!query}
+            message={message}
+            nextCursor={bookmarksResponse.cursor_info.next_cursor}
+            prevCursor={bookmarksResponse.cursor_info.previous_cursor}
+          />
+          {(drillTags.length > 0 || hasUntagged) && (
+            <DrillDownCard tags={drillTags} showUntagged={hasUntagged} />
+          )}
+          <ul className="flex flex-col">
+            {bookmarksResponse.bookmarks.map((bookmark) => (
+              <BookmarkRow key={bookmark.id} bookmarkId={bookmark.id} />
+            ))}
+          </ul>
+        </div>
+        <RefreshOnFocus />
+        <WaitForMutations />
+      </HydrationBoundary>
+    )
+  },
+  { returnTo: "/" }
+)
--- src/app/(bookmarks)/tags/page.tsx
+++ src/app/(bookmarks)/tags/page.tsx
@@ -3,24 +3,27 @@ import TagFilter from "./tag-filter"
 import { RefreshOnFocus } from "../refresh-on-focus"
 import { Metadata } from "next"
 import ErrorScreen from "../error-screen"
+import { withPageAuthRequired } from "@auth0/nextjs-auth0"
 
 export const metadata: Metadata = {
   title: "Tags – Savory",
 }
 
-export default async function TagsPage() {
-  let tagsResponse
-  try {
-    tagsResponse = await bapi.getTagsCount()
-  } catch (error) {
-    // TODO: If error is logout, redirect!
-    return <ErrorScreen />
-  }
+export default withPageAuthRequired(
+  async function TagsPage() {
+    let tagsResponse
+    try {
+      tagsResponse = await bapi.getTagsCount()
+    } catch (error) {
+      return <ErrorScreen />
+    }
 
-  return (
-    <div>
-      <TagFilter tags={tagsResponse} />
-      <RefreshOnFocus />
-    </div>
-  )
-}
+    return (
+      <div>
+        <TagFilter tags={tagsResponse} />
+        <RefreshOnFocus />
+      </div>
+    )
+  },
+  { returnTo: "/tags" }
+)
