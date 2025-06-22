c82240d569389544378c454673dd759ae626c033
Akshay Kumar <akshaykumar90@gmail.com>
2025-06-22 10:40:23 +0530

    Fix refresh on focus bug (#118)

--- src/app/(bookmarks)/refresh-on-focus.tsx
+++ src/app/(bookmarks)/refresh-on-focus.tsx
@@ -1,21 +1,20 @@
 "use client"
 
-import { useRouter, useSearchParams } from "next/navigation"
+import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
 export function RefreshOnFocus() {
   const { refresh } = useRouter()
-  const searchParams = useSearchParams()
-
-  const isSearchPage = !!searchParams.get("q")
 
   useEffect(() => {
-    if (isSearchPage) {
-      return
-    }
-
     const onFocus = () => {
-      refresh()
+      // Check if we're on a search page at the time of focus
+      const currentSearchParams = new URLSearchParams(window.location.search)
+      const isCurrentlySearchPage = !!currentSearchParams.get("q")
+
+      if (!isCurrentlySearchPage) {
+        refresh()
+      }
     }
 
     window.addEventListener("focus", onFocus)
@@ -23,7 +22,7 @@ export function RefreshOnFocus() {
     return () => {
       window.removeEventListener("focus", onFocus)
     }
-  }, [refresh, isSearchPage])
+  }, [refresh])
 
   return null
 }
